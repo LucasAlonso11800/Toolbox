d70745cce307aca8ca143fff113f7ef6e77db639
LucasAlonso11800 <lucasalonso11800@gmail.com>
2023-12-13 10:49:10 -0300

    Added header and filename filter

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,10 +3,15 @@ import axios from "axios";
 import Spinner from "react-bootstrap/Spinner";
 import Table from "react-bootstrap/Table";
 import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import Row from "react-bootstrap/Row";
 
 export default function App() {
   const [files, setFiles] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [fileName, setFileName] = useState("");
 
   const getFiles = useCallback(async (fileName = "") => {
     try {
@@ -29,10 +34,18 @@ export default function App() {
   if (loading)
     return (
       <>
-        <header></header>
+        <Navbar className="bg-danger mb-4">
+          <Navbar.Brand className="mx-3 text-white">
+            React Test App
+          </Navbar.Brand>
+        </Navbar>
         <main>
           <Container>
-            <Spinner animation="border" variant="primary" className="d-block mx-auto mt-4"/>
+            <Spinner
+              animation="border"
+              variant="primary"
+              className="d-block mx-auto"
+            />
           </Container>
         </main>
       </>
@@ -40,10 +53,35 @@ export default function App() {
 
   return (
     <>
-      <header></header>
+      <Navbar className="bg-danger mb-4">
+        <Navbar.Brand className="ms-3 text-white">React Test App</Navbar.Brand>
+      </Navbar>
       <main>
         <Container>
-          <Table striped bordered hover className="mt-4">
+          <Form
+            className="mb-4"
+            onSubmit={(e) => {
+              e.preventDefault();
+              getFiles(fileName);
+            }}
+          >
+            <Form.Group className="mb-2">
+              <Form.Label>Search a specific file</Form.Label>
+              <Row >
+                <Form.Control
+                  type="text"
+                  placeholder="File name"
+                  value={fileName}
+                  onChange={(e) => setFileName(e.target.value)}
+                  className="d-inline w-auto"
+                />
+                <Button variant="primary" type="submit" className="ms-2 w-auto">
+                  Search
+                </Button>
+              </Row>
+            </Form.Group>
+          </Form>
+          <Table striped bordered hover>
             <thead>
               <tr>
                 <th>File Name</th>
